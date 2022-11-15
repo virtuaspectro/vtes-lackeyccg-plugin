7b579ce0f8660488f51dc60f262be545db7b90b0
Lionel Panhaleux <lionel.panhaleux@gmail.com>
2022-11-15 16:14:41 +0100

    Cards list generation: simplify script

--- cardgen/__main__.py
+++ cardgen/__main__.py
@@ -1,61 +1,53 @@
+from krcg import utils
 from krcg import vtes
 from unidecode import unidecode
 
 
-def _write_card(f, c):
-    filename = c.url.rsplit("/", 1)[1].rsplit(".", 1)[0]
-    cardname = unidecode(c._name)
-    if c.crypt:
-        filename += f",cardbackcrypt"
-        disciplines = " ".join(c.disciplines)
-        suffixes = []
-        if c.is_evolution:
-            suffixes.append(f"G{c.group}")
-        if c.adv:
-            suffixes.append("ADV")
-        if suffixes:
-            cardname += f" ({' '.join(suffixes)})"
-    else:
-        if c.combo:
-            separator = " & "
-        else:
-            separator = "/"
-        disciplines = separator.join(c.disciplines)
-    card_text = c.card_text.replace("\t", " ").replace("\n", " ")
-    card_text = card_text.replace("/", "").replace("{", "").replace("}", "")
-    card_text = card_text.replace("Ⓓ", "(D)")
-    f.write(
-        "\t".join(
-            [
-                cardname,
-                c.ordered_sets[-1],
-                filename,
-                "/".join(c.types),
-                "/".join(c.clans),
-                str(c.group or ""),
-                str(c.capacity or ""),
-                disciplines,
-                str(c.pool_cost or ""),
-                str(c.blood_cost or ""),
-                unidecode(card_text),
-                c._set,
-            ]
-        )
-        + "\n"
-    )
+def pack_line(*args):
+    return "\t".join(args) + "\n"
 
 
-def main():
+def yield_cards_data():
     vtes.VTES.load_from_vekn()
-    with open("plugin/sets/allsets.txt", "w") as f:
-        f.write(
-            "Name	Set	ImageFile	Type	Clan	Group	Capacity	Discipline	PoolCost	BloodCost	Text	Rarity\n"
+    yield pack_line(
+        "Name",
+        "Set",
+        "ImageFile",
+        "Type",
+        "Clan",
+        "Group",
+        "Capacity",
+        "Discipline",
+        "PoolCost",
+        "BloodCost",
+        "Text",
+        "Rarity",
+    )
+    for c in sorted(vtes.VTES, key=lambda c: (c.library, utils.normalize(c._name))):
+        filename = c.url.rsplit("/", 1)[1].rsplit(".", 1)[0]
+        if c.crypt:
+            filename += f",cardbackcrypt"
+            disciplines = " ".join(c.disciplines)
+        else:
+            disciplines = (" & " if c.combo else "/").join(c.disciplines)
+        card_text = c.card_text.replace("\t", " ").replace("\n", " ")
+        card_text = card_text.replace("/", "").replace("{", "").replace("}", "")
+        card_text = card_text.replace("Ⓓ", "(D)")
+        yield pack_line(
+            unidecode(c.vekn_name),
+            c.ordered_sets[-1],
+            filename,
+            "/".join(c.types),
+            "/".join(c.clans),
+            str(c.group or ""),
+            str(c.capacity or ""),
+            disciplines,
+            str(c.pool_cost or ""),
+            str(c.blood_cost or ""),
+            unidecode(card_text),
+            c._set,
         )
-        cards = sorted(
-            vtes.VTES, key=lambda c: (c.library, unidecode(c._name).casefold())
-        )
-        for c in cards:
-            _write_card(f, c)
 
 
-main()
+with open("plugin/sets/allsets.txt", "w") as f:
+    f.writelines(yield_cards_data())
